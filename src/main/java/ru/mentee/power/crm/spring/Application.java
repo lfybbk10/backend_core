c1671218bdc330cdb20b5149d5c22ea247877bdd
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2026-01-17 08:39:36 +0300

    BCORE-16: Интеграционный тест Servlet vs Spring Boot

--- src/main/java/ru/mentee/power/crm/spring/Application.java
+++ src/main/java/ru/mentee/power/crm/spring/Application.java
@@ -1,11 +1,25 @@
 package ru.mentee.power.crm.spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.mentee.power.crm.service.LeadService;
 
 @SpringBootApplication(scanBasePackages = "ru.mentee.power.crm")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner seedLeads(LeadService leadService) {
+        return args -> {
+            leadService.addLead("test1@mail.com", "comp1", "NEW");
+            leadService.addLead("test2@mail.com", "comp2", "NEW");
+            leadService.addLead("test3@mail.com", "comp3", "NEW");
+            leadService.addLead("test4@mail.com", "comp2", "NEW");
+            leadService.addLead("test5@mail.com", "comp1", "NEW");
+        };
     }
 }
